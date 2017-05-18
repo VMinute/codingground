fb439867d9b1263084403f2c12e0f8eff3685085
Valter Minute <valter.minute@gmail.com>
2017-05-18 21:51:36 +0200

    Create main.cpp

---
+++ pe21/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int sumOfDivisors(int num)
+{
+    int sum=1;
+    int max=sqrt(num);
+    
+    for (int i=2;i<num;i++)
+    {
+        if ((num % i)==0)
+            sum+=i;
+    }
+    
+    return sum;
+}
+
+int main()
+{
+    int tot=0;
+    
+    for (int i=1;i<10000;i++)
+    {
+        int sum1=sumOfDivisors(i);
+        
+        // we already calculated it
+        if (sum1<=i)  
+            continue;
+    
+        int sum2=sumOfDivisors(sum1);
+        
+        if (i!=sum2)
+            continue;
+            
+        if (sum2>10000)
+            continue;
+            
+        cout << i << "-" << sum1 << "\r\n";
+        
+        tot+=i+sum1;
+    }
+    
+    cout << tot;
+    return 0;
+}
